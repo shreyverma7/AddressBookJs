73498cfeea4b28a96e3fb393782dfc83ac06babd
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-09-12 16:26:01 +0530

    [Shrey] Add :Abiltity to add new contact in array

--- Operation.js
+++ Operation.js
@@ -4,10 +4,14 @@ const AddressBook = require('./AddressBook');
 // Usage example:
 const addressBook = new AddressBook();
 
-const contact1 = new Contact("John", "Doe", "123 Main St", "New York", "NY", "10001", "555-555-5555", "john@example.com");
-const contact2 = new Contact("Jane", "Smith", "456 Elm St", "Los Angeles", "CA", "90001", "555-555-1234", "jane@example.com");
+const contact1 = new Contact("John", "Doe", "123 Main St", "New York", "Tamil", "100001", "1234567890", "john@example.com");
+const contact2 = new Contact("Jane", "Smith", "456 Elm St", "Los Angeles", "Andhra", "900001", "1234567890", "jane@example.com");
+const contact3 = new Contact("Shrey", "Vermas", "123 adsassvs", "Sasffls", "CAasd", "122345", "1232131232", "shrey@example.com");
+
 
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
+addressBook.addContact(contact3);
+
 
 addressBook.displayContacts();
