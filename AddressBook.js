5e6399ba2237cbd7c64255035497a8b74c3a7665
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-09-12 17:04:44 +0530

    [Shrey] Add :Abiltity to edit contact with name

--- AddressBook.js
+++ AddressBook.js
@@ -22,6 +22,30 @@ class AddressBook {
             console.log("-------------"); // Separator between contacts
         });
     }
+    findContactByName(firstName, lastName) {
+        const foundContactIndex = this.contacts.findIndex(contact => {
+            return contact.firstName === firstName && contact.lastName === lastName;
+        });
+
+        if (foundContactIndex !== -1) {
+            return this.contacts[foundContactIndex];
+        } else {
+            return null;
+        }
+    }
+
+    editContact(firstName, lastName, updatedContactInfo) {
+        const foundContactIndex = this.contacts.findIndex(contact => {
+            return contact.firstName === firstName && contact.lastName === lastName;
+        });
+
+        if (foundContactIndex !== -1) {
+            this.contacts[foundContactIndex] = { ...this.contacts[foundContactIndex], ...updatedContactInfo };
+            return true; 
+        } else {
+            return false; 
+        }
+    }
 }
 
 module.exports = AddressBook;
--- Operation.js
+++ Operation.js
@@ -8,10 +8,34 @@ const contact1 = new Contact("John", "Doe", "123 Main St", "New York", "Tamil", 
 const contact2 = new Contact("Jane", "Smith", "456 Elm St", "Los Angeles", "Andhra", "900001", "1234567890", "jane@example.com");
 const contact3 = new Contact("Shrey", "Vermas", "123 adsassvs", "Sasffls", "CAasd", "122345", "1232131232", "shrey@example.com");
 
-
+//Adding contact
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
 addressBook.addContact(contact3);
 
+//Display
+addressBook.displayContacts();
+//search contact
+const foundContact = addressBook.findContactByName("Shrey", "Vermas");
 
+if (foundContact) {
+    // update contact
+    const updatedInfo = {
+        address: "789 Elm St",
+        city: "Chicago",
+        state: "ILqwerty",
+        zip: "600601",
+        phoneNumber: "1234567890",
+        email: "updated.email@example.com",
+    };
+    const success = addressBook.editContact("Shrey", "Vermas", updatedInfo);
+    if (success) {
+        console.log("Contact updated successfully!");
+    } else {
+        console.log("Contact not found.");
+    }
+}
+else {
+    console.log("Contact not found.");
+}
 addressBook.displayContacts();
